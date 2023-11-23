972802cf7cd1aab5a22b930f12c87acbe8cceb84
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2023-11-23 17:14:00 +0530

    data

--- schema.sql
+++ schema.sql
@@ -6,4 +6,24 @@ CREATE TABLE IF NOT EXISTS customer(
     country VARCHAR(50) NOT NULL
 );
 
-INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
+
+
+
+INSERT INTO customer (first_name, last_name, contact, country) VALUES
+('John', 'Doe', '077-1234567', 'Sri Lanka'),
+('Alice', 'Smith', '077-2345678', 'Sri Lanka'),
+('Bob', 'Johnson', '077-3456789', 'Sri Lanka'),
+('Eva', 'Williams', '077-9876543', 'Sri Lanka'),
+('Jack', 'Miller', '077-1112233', 'Sri Lanka'),
+('Emily', 'Davis', '077-4455667', 'Sri Lanka'),
+('Charlie', 'Clark', '077-7788990', 'Sri Lanka'),
+('Olivia', 'Anderson', '077-1122334', 'Sri Lanka'),
+('James', 'Taylor', '077-4455667', 'Sri Lanka'),
+('Sophia', 'Moore', '077-7788990', 'Sri Lanka'),
+('Liam', 'White', '077-1122335', 'Sri Lanka'),
+('Ava', 'Johnson', '077-4455668', 'Sri Lanka'),
+('Noah', 'Brown', '077-7788991', 'Sri Lanka'),
+('Isabella', 'Jones', '077-1122336', 'Sri Lanka'),
+('Mason', 'Davis', '077-4455669', 'Sri Lanka'),
+('Emma', 'Taylor', '077-7788992', 'Sri Lanka');
+
